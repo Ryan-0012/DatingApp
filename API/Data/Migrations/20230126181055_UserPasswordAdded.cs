d5d19e479119217c2530758c4d088d53b6e48d3f
Ryan-0012 <ryanfreitas1229@gmail.com>
2023-01-31 22:54:31 -0300

    End of section 4

---
+++ API/Data/Migrations/20230126181055_UserPasswordAdded.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassawordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassawordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PassawordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PassawordSalt",
+                table: "Users");
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PassawordHash { get; set; }
+        public byte[] PassawordSalt { get; set; }
+
+        
     }
 }
