a1b0337193d2b7d3771fb25005b7bb457076c80e
Ryan-0012 <ryanfreitas1229@gmail.com>
2023-03-30 12:54:07 -0300

    End of section 9

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -12,21 +12,15 @@ export class HomeComponent {
 
   constructor(private http: HttpClient){}
 
-  ngOnInit(){
-    this.getUsers();
+  ngOnInit(): void {
+ 
   }
 
   registerToggle(){
     this.registerMode = !this.registerMode;
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error =>console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+  
 
   cancelRegisterMode(event:  boolean){
     this.registerMode = event;
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery/public-api';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,5 +10,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent {
+  member: Member | undefined;
+  galleryOptions: NgxGalleryOptions[] = [];
+  galleryImages: NgxGalleryImage[] = [];
+
+  constructor(private memberService: MembersService, private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.loadMember();
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+    
+  }
+
+  getImages() {
+    if(!this.member) return [];
+    const imageUrl = [];
+    for(const photo of this.member.photos){
+      imageUrl.push({
+        small: photo.url,
+        medium: photo.url,
+        big: photo.url
+      })
+    }
+    return imageUrl;
+  }
+
+  loadMember(){
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return; 
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member;
+        this.galleryImages = this.getImages();
+      }
+    })
+  }
+
 
 }
